8e8aefb2fc857fca333c681d5769ff01036f19f7
Hiren Garai <hirengarai@gmail.com>
2021-12-30 12:01:00 +0530

    na

--- pnbiascheck.cpp
+++ pnbiascheck.cpp
@@ -9,20 +9,14 @@
 using namespace std;
 
 #define u32 uint32_t
-#define ul unsigned int
+#define ull unsigned long long
 
 #define ROTL(x, b) (((x) >> (32 - (b))) | ((x) << (b)))
 #define ROTR(x, b) (((x) << (32 - (b))) | ((x) >> (b)))
 
-#define myrand32 ((ul)(4294967296.0 * drand48()))
-
-#define ull unsigned long long
-#define mod 4294967296
-#define MOD(x) ((x + mod) % mod)
-
-ul GetRandomNumber32()
+u32 GetRandomNumber32()
 {
-       ull temp;
+       u32 temp;
        int i;
        if (drand48() < 0.5)
               temp = 0;
@@ -38,17 +32,7 @@ ul GetRandomNumber32()
        }
        return temp;
 }
-void print(ul *x)
-{
-       int i;
-       for (i = 0; i < 4; i++)
-       {
-              printf("%8x ", x[i]);
-              if (i > 0 && i % 4 == 3)
-                     printf("\n");
-       }
-       printf("\n");
-}
+
 void inti(u32 *v)
 {
        int i;
@@ -58,19 +42,14 @@ void inti(u32 *v)
               v[i] = GetRandomNumber32();
        }
 }
-void initializerKey(ul *k)
+void initializerKey(u32 *k)
 {
        for (int i = 0; i < 4; i++)
        {
               k[i] = GetRandomNumber32();
        }
 }
-void diffinitializeR(ul *x, int w, int b)
-{
-       int i;
-       int pattern = 0x00000001 << b;
-       x[w] = x[w] ^ pattern;
-}
+
 void copystate(u32 *v1, u32 *v)
 {
        int i;
@@ -84,7 +63,7 @@ void E(u32 *v, u32 *k)
 
        int i;
 
-       for (i = 0; i < 3; i++)
+       for (i = 0; i < 3; ++i)
        {
               v[0] += v[1];
               v[1] = ROTL(v[1], 5);
@@ -152,7 +131,7 @@ void fshalf(u32 *v)
        v[2] = ROTL(v[2], 16);
 }
 /*Backward Rounds*/
-void D(u32 *v, u32 *k)
+void D1(u32 *v, u32 *k)
 { /*round function*/
 
        int i;
@@ -188,7 +167,7 @@ void bshalf(u32 *v)
        v[3] = ROTR(v[3], 13);
        v[0] -= v[3];
 }
-ul *xordiff(ul *v, ul *v1, ul *z)
+u32 *xordiff(u32 *v, u32 *v1, u32 *z)
 {
        for (int j = 0; j < 4; ++j)
        {
@@ -200,13 +179,13 @@ ul *xordiff(ul *v, ul *v1, ul *z)
 int main()
 {
        srand48(time(NULL));
-       u32 v[4], v1[4], k[4], v2[4], y[4], y1[4], y2[4], z[4], z1[4];
+       u32 v[4], v1[4], k[4], v2[4], z[4], z1[4];
        // diffForward, diffBack;
-       ul d[4], r[4];
-       int PNB_SET[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 20, 21, 22, 25, 32, 33, 34, 35, 36, 39, 40, 43, 44, 45, 46, 51, 52, 53, 58, 59, 60, 61, 62, 63, 67, 68, 69, 74, 75, 76, 77, 78, 79, 80, 81, 84, 87, 92, 93, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118,121};
-  
+       u32 d[4], r[4];
+       int PNB_SET[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 20, 21, 22, 25, 32, 33, 34, 35, 36, 39, 40, 43, 44, 45, 46, 51, 52, 53, 58, 59, 60, 61, 62, 63, 67, 68, 69, 74, 75, 76, 77, 78, 79, 80, 81, 84, 87, 92, 93, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 121};
+
        int ll = 80;
-       ul FORWARD_DIFF, BACK_DIFF, pattern;
+       u32 FORWARD_DIFF, BACK_DIFF, pattern;
        int i, j, WORD, BIT;
        double cnt = 0.0;
 
@@ -221,11 +200,9 @@ int main()
        int e = 0;
        double round = 1024 * 64;
        ull loop = 0;
-       ull LOOP = 0;
 
-       while (LOOP < 1024 * 1024 * 1024)
+       while (loop < 32*1*1)
        {
-
               inti(v);
               initializerKey(k);
               for (int i = 0; i < 4; i++)
@@ -233,11 +210,8 @@ int main()
                      v[i] ^= k[i];
               }
               copystate(v1, v);
-              copystate(y, v);
-
-              ul pattern = (0x00000001 << 31);
+              u32 pattern = (0x00000001 << 31);
               v1[0] = v[0] ^ pattern;
-              copystate(y1, v1);
               fshalf(v);
               fshalf(v1);
               E(v, k);
@@ -259,12 +233,7 @@ int main()
                      v[i] ^= k[i];
                      v1[i] ^= k[i];
               }
-
-              for (int i = 0; i < 4; i++)
-              {
-                     v[i] ^= k[i];
-                     v1[i] ^= k[i];
-              }
+//randomising
               for (j = 0; j < ll; j++)
               {
 
@@ -273,31 +242,34 @@ int main()
 
                      if (drand48() < 0.5)
                      {
-                            y[WORD] = y[WORD] ^ (0x00000001 << BIT);
-                            y1[WORD] = y1[WORD] ^ (0x00000001 << BIT);
+                            k[WORD] = k[WORD] ^ (0x00000001 << BIT);
                      }
               }
-              for (int round = 6; round > 5; --round)
+
+              for (int i = 0; i < 4; i++)
               {
-                     D(v, k);
-                     D(v1, k);
+                     v[i] ^= k[i];
+                     v1[i] ^= k[i];
               }
+
+              D1(v, k);
+              D1(v1, k);
               bshalf(v);
               bshalf(v1);
               xordiff(v, v1, d);
 
               BACK_DIFF = (d[2]);
 
-              ul pattern0 = (0x00000001 << 20);
+              u32 pattern0 = (0x00000001 << 20);
               if (((FORWARD_DIFF ^ BACK_DIFF) & pattern0) == 0)
               {
 
                      cnt = cnt + 1.0;
-
                      loop++;
               }
+              
               if (loop > 0 && loop % (2) == 0)
-                     printf("%llu   %0.10f   \n", loop / (1024 * 256), 2 * ((cnt / loop) - 0.5));
+              printf("%llu   %0.10f   \n", loop / (1024), 2 * ((cnt / loop) - 0.5));
        }
 }
 //                      cnt = cnt + 1.0;
